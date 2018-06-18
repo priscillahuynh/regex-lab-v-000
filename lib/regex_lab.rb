902301d4b6580471e7059a92ce2d6d5d42785501
Priscilla H <priscillamhuynh@gmail.com>
2018-06-18 09:18:18 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiou]/i)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+   text.scan(/\A^un/ && /\zing/) 
 end
 
 def words_five_letters_long(text)
